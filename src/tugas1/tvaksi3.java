531127f6870ce93cc9b6f4597ac06de89b86aaaf
shandy <shandysaputra00@gmail.com>
2018-04-06 19:45:34 +0800

    memanggil method3

---
+++ src/tugas1/tvaksi3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author shandy
+ */
+public class tvaksi3 {
+    public static void main(String[] args) {
+        tv samsung = new tv();
+        
+        samsung.type="samsung 003";
+        samsung.ukuran="40 inch";
+        samsung.harga="3.500.000";
+        samsung.warna="hitam";
+        samsung.jenis="LCD";
+        
+        samsung.CetakInfo();
+    }
+}
