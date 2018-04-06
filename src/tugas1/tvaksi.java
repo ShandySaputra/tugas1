08662b4cf40ea52a28b95d1ac6445e493ac4847c
shandy <shandysaputra00@gmail.com>
2018-04-06 14:19:44 +0800

    memanggil method

---
+++ src/tugas1/tvaksi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author shandy
+ */
+public class tvaksi {
+public static void main(String[] args) {
+        tv sony = new tv();
+        
+        sony.type="Muhammad aja";
+        sony.ukuran="Baju Kuning";
+        sony.harga="Celana Merah";
+        sony.warna="Kaos Kaki hitam";
+        sony.jenis="Sepatu Hitam";
+        
+        sony.CetakInfo();
+    }    
+}
